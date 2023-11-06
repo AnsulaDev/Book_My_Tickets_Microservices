e07fa1cc86df4589c6aa62dcd9976f8b515520f7
AnsulaDev <ansulaoari88@gmail.com>
2023-11-06 20:23:55 +0530

    fixing errors for signup page and some custom hooks

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,22 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
 export default () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const { makeRequest, errors} = useRequest({
-        url:'/api/users/signup',
-        method:'post',
-        body:{
-            email, password
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+        email,
+        password
         },
         onSuccess: () => Router.push('/')
     });
+
     const onSubmit = async event => {
         event.preventDefault();
-        
-        await makeRequest();
+
+        await doRequest();
     };
 
     return (
